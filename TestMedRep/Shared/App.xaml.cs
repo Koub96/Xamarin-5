2fd2933b2f37d1bc9ae8eed749cf3f1579b5e818
Koub96 <46316600+Koub96@users.noreply.github.com>
2021-09-17 17:18:05 +0300

    Add files via upload

---
+++ TestMedRep/Shared/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace test.universal
+{
+	public partial class App : Application
+	{
+        public App()
+		{
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+    }
+
+}
---
+++ TestMedRep/Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace test.universal
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage ()
+		{
+            InitializeComponent();
+
+            
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Application.Current.MainPage.DisplayAlert("Button1", "Button2", "Button3", "Button4");
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
